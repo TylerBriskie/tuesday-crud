843d2a6d1eae2ceb1980687fe575f22df645e099
Tyler Briskie <tylerbriskie@gmail.com>
2016-12-14 11:54:40 -0700

    validation.........

--- client/scripts/add_book.js
+++ client/scripts/add_book.js
@@ -8,24 +8,24 @@ $(document).ready(function(){
     newBook.genre = $('#book_genre').val();
     newBook.page_count = $('#book_pages').val();
     newBook.image_url = $('#cover_art').val();
-    if(newBook.title.length ===0|| newBook.author.length ===0|| newBook.genre.length===0
-      || newBook.page_count.length ===0 ){
+    if(newBook.title.length === 0 || newBook.author.length === 0 || newBook.genre.length === 0
+      || newBook.page_count.length === 0 ){
         alert("Please Enter All Fields");
-      } else if (typeof Number(newBook.page_count) !== 'number'){
+      } else if (isNaN(newBook.page_count)){
         alert("Make sure page count is a NUMBER");
       } else {
-      $.ajax({
-        url:'https://tdb-library.herokuapp.com/books/',
-        type:'POST',
-        data:newBook,
-        success: function(result){
-          alert("Book successfully added to database!");
-          window.location='/books.html'
-        },
-        fail: function(error){
-          console.log(error);
-        }
-      });
+        $.ajax({
+          url:'https://tdb-library.herokuapp.com/books/',
+          type:'POST',
+          data:newBook,
+          success: function(result){
+            alert("Book successfully added to database!");
+            window.location='/books.html'
+          },
+          fail: function(error){
+            console.log(error);
+          }
+        });
     }
   });
 
