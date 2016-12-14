c3a284853af8669fafc3c26e4a3763ccb3384e43
Tyler Briskie <tylerbriskie@gmail.com>
2016-12-14 10:56:09 -0700

    adding placeholder image for empty image_url field

--- client/scripts/add_book.js
+++ client/scripts/add_book.js
@@ -13,7 +13,9 @@ $(document).ready(function(){
         alert("Please Enter All Fields");
       } else if (typeof newBook.page_count !== 'number'){
         alert("Make sure page count is a NUMBER");
-      } else {
+      } else if (newBook.image_url.length===0){
+        newBook.image_url = "http://www.lisahughey.com/wp-content/uploads/2016/10/placeholder-cover-1.jpg"
+      }else {
       $.ajax({
         url:'https://tdb-library.herokuapp.com/books/',
         type:'POST',
